121222666949cfa3bfe94e2b5ed424ed2ac6f848
Jonathan Bossio <bossiojonathand@gmail.com>
2023-10-05 17:39:55 +0200

    Various improvements (make it more realistic)
    Also:
    - Improve data generation (add more noise, produce only few points,
      variable number of points per class)
    - Add plot comparing class distribution between data samples
    - Improve readability (improve comments and add type annotations)
    - Remove unnecessary to_categorical transformation (adapted loss
      definition to use sparse_categorical_crossentropy instead)
    - Using standardization instead of normalization
    - Standardization is done separately for each dataset (testing,
      training, validation)
    - Validation data now done by hand using train_test_split() function
    - Performance (true vs predicted labels) is now done only using testing
      data (not full data)
    - Add confusion matrix

--- README.md
+++ README.md
@@ -22,9 +22,10 @@ For generating the data, training the model and evaluating the peformance in tes
 python multiclass_classifier_keras.py
 ```
 
-This will save the best trained model to ```best_model.h5``` and will create four PDFs:
+This will save the best trained model to ```best_model.h5``` and will create five PNG images:
 
-- ```data.pdf```: this is a scatter plot with the generated data
-- ```data_normalized.pdf```:  this is the same as ```data.pdf``` but here the data was normalized
-- ```data_predicted.pdf```: the same normalized data is plotted but the colors are based on the predicted labels (if the DNN works well it should look very similar to ```data_normalized.pdf```)
-- ```loss.pdf```: this will have the loss and val_loss vs epoch 
+- ```data.png```: this is a scatter plot with the generated data
+- ```data_normalized.png```:  this is the same as ```data.png``` but here the data was normalized
+- ```data_predicted.png```: the same normalized data is plotted but the colors are based on the predicted labels (if the DNN works well it should look very similar to ```data_normalized.png```)
+- ```loss.png```: this will have the loss and val_loss vs epoch
+- ```compare_distribution_of_classes_data.png```: this compares the distribution of classes for each data set type (training, test, validation)
--- multiclass_classifier_keras.py
+++ multiclass_classifier_keras.py
@@ -4,35 +4,72 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy.random import uniform, choice, normal
 from tensorflow import keras
 from keras.layers import Dense, Dropout
 from keras import Sequential
 from keras import optimizers
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
-def create_data(k: int, npoints: int):
+# Setting the seed with keras.utils.set_random_seed. This will set:
+# 1) `numpy` seed
+# 2) `tensorflow` random seed
+# 3) `python` random seed
+keras.utils.set_random_seed(42)
+
+
+def create_data(
+        k: int,  # number of classes
+        npoints: int,  # total number of points to be generated
+        seed: int = 1  # seed for reproducibility
+    ) -> tuple[np.array, np.array]:
     """
-    Create random data
-     k: number of clusters/classes
-     npoints: total number of points to be generated
+    Randomly create npoints data points classified into k classes
     """
-    np.random.seed(1)
-    X = []
-    labels = []
-    for i in range(k):
-        center_year = np.random.uniform(0, 100)
-        center_income = np.random.uniform(0, 1000000)
-        npoints = int(npoints / k)
-        for j in range(npoints):
-            labels.append(i)
-            X.append([np.random.normal(center_year, 1), np.random.normal(center_income, 10000)])
+    # Set seed for reproducibility
+    np.random.seed(seed)
+    X = []  # collect data
+    labels = []  # collect class labels
+    
+    # Randomly define a centroid for each class
+    age_centers = {i: uniform(25, 100) for i in range(k)}
+    income_centers = {i: uniform(0, 1000000) for i in range(k)}
+    
+    # Generate npoints
+    for ipoint in range(npoints):
+        # Randomly assign this point to a class
+        ik = choice(range(k))
+        labels.append(ik)
+        
+        # Retrieve centroid for this class
+        center_age = age_centers[ik]
+        center_income = income_centers[ik]
+        
+        # Generate point
+        X.append([
+            normal(center_age, 3),
+            normal(center_income, 15000)
+        ])
+
     return np.array(X), np.array(labels)
 
 
-def make_scatter_plot(data, labels, k, outname):
+def make_scatter_plot(
+        data: np.array,  # 2D array (age, income)
+        labels: np.array,  # 1D array with class labels
+        k: int,  # number of classes
+        outname: str  # output name
+    ) -> None:
     """ Make scatter plot"""
+    
+    # Protection
+    assert k < 9, "Only up to 8 classes are supported"
+    
+    # Define colors for each class
     colors = {
         0: 'red',
         1: 'black',
@@ -43,20 +80,39 @@ def make_scatter_plot(data, labels, k, outname):
         6: 'magenta',
         7: 'cyan',
     }
-    plt.figure(outname.replace('.pdf', ''))
-    for ik in range(k):
+
+    # Plot data from each class
+    title = outname.split('.')[0]
+    plt.figure(title)
+    plt.title(title)
+    for ik in range(k):  # loop over classes
+        # Get data for class ik      
         data_k = data[labels == ik]
+
+        # Unpack data (age and income)
         X = list(zip(*data_k))
         x, y = X[0], X[1]
+
+        # Make scatter plot and add x- and y-axis titles
         plt.scatter(x, y, c = colors[ik])
-    plt.savefig(outname, format = 'pdf')
+        plt.xlabel('Age')
+        plt.ylabel('Income')
+
+    # Save figure
+    plt.savefig(outname)
 
 
-def create_model(k: int):
-    """
-    Create Sequential model for multiclass classifier
-     k: number of clusters/classes
-    """
+def standardize_data(data: tuple[np.array, np.array]) -> tuple[np.array, np.array]:
+    """ Standardize data using StandardScaler """
+    scaler = StandardScaler()
+    data_standardized = scaler.fit_transform(data)
+    return data_standardized
+
+
+def create_model(
+        k: int  # number of classes
+    ) -> Sequential:
+    """ Create Sequential model for multiclass classifier """
     model = Sequential()
     model.add(Dense(16, activation='relu', input_dim=2))
     model.add(Dropout(0.5))
@@ -66,40 +122,102 @@ def create_model(k: int):
     return model
 
 
-def plot_loss(history):
+def plot_loss(history) -> None:
     """ Plot loss and val_loss """
     history_df = pd.DataFrame(history.history)
     plt.figure('loss')
-    plt.plot(history_df['loss'], label='loss', c = 'black')
-    plt.plot(history_df['val_loss'], label='val_loss', c = 'red')
+    plt.plot(history_df['loss'], label='loss', c='black')
+    plt.plot(history_df['val_loss'], label='val_loss', c='red')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
     plt.legend()
-    plt.savefig("loss.pdf")
+    plt.savefig('loss.png')
 
 
-def compile(model, optimizer = 'Adam'):
+def compare_distributions(hists, k) -> None:
+    """ Compare distributions """
+    
+    # Protection
+    assert k < 9, "Only up to 8 classes are supported"
+
+    # Define a color for every class
+    colors = {
+        0: 'red',
+        1: 'black',
+        2: 'blue',
+        3: 'green',
+        4: 'yellow',
+        5: 'orange',
+        6: 'magenta',
+        7: 'cyan',
+    }
+    
+    # List of data categories
+    data_categories = hists.keys()
+    
+    # Prepare the data
+    data_dict = {}
+    for i, (data_type, data) in enumerate(hists.items()):
+        _, values = np.unique(data, return_counts=True)  # get counts for every class
+        data_dict[data_type] = values
+ 
+    # Create numerical axis
+    x = np.arange(k)
+
+    # Set the width of the bars
+    width = 0.2
+
+    # Create figure and axis
+    fig, ax = plt.subplots()
+
+    # Create bars
+    for i, (data_type, counts) in enumerate(data_dict.items()):
+        offset = width * i  # offset in the x-axis (different for each bar)
+        bar = ax.bar(x + offset, counts, width=width, label=data_type, color=colors[i])
+        ax.bar_label(bar)  # show numbers on top of bars
+
+    # Label the axes
+    ax.set_xlabel('Class')
+    ax.set_ylabel('Counts')
+
+    # Show category names
+    ax.set_xticks(x + width, list(range(k)))
+
+    # Add legends
+    ax.legend()
+
+    # Save figure
+    fig.savefig('compare_distribution_of_classes_data.png')
+
+
+def compile(model: Sequential, optimizer: str = 'Adam') -> None:
     """ Compile model """
-    opt = optimizers.Adam(learning_rate = 0.001) if optimizer == 'Adam' else keras.optimizers.SGD(learning_rate=0.1)
+    opt = optimizers.Adam(learning_rate=0.001) if optimizer == 'Adam' else optimizers.SGD(learning_rate=0.1)
     model.compile(
-        loss='categorical_crossentropy',
+        loss = 'sparse_categorical_crossentropy',
         optimizer = opt,
         metrics = ['accuracy']
     )
 
 
-def train(model, x_train, y_train):
+def train(model, x_train, y_train, val_data):
     """ Train model """
+    # Define callbacks
     callbacks = [
+        # EarlyStopping: Stop training when a val_loss stops improving
         keras.callbacks.EarlyStopping(
-            patience = 5,
+            min_delta = 0.005,
+            patience = 10,
             monitor = 'val_loss',
             mode = 'min',
             restore_best_weights = True
         ),
+        # ModelCheckpoint: Save model w/ lowest val_loss
         keras.callbacks.ModelCheckpoint(
             'best_model.h5',
-            monitor='val_loss',
+            monitor = 'val_loss',
             mode = 'min',
-            verbose=0,
+            verbose = 0,
             save_best_only = True
         ),
     ]
@@ -107,12 +225,14 @@ def train(model, x_train, y_train):
         x_train,
         y_train,
         epochs = 300,
-        validation_split = 0.2,
-        callbacks = callbacks,
+        validation_data = val_data,
+        callbacks = callbacks
     )
 
 
-def create_and_compile_model(k):
+def create_and_compile_model(
+        k: int  # number of classes
+    ) -> Sequential:
     """ Create and compile model """
     # Create model
     model = create_model(k)
@@ -124,66 +244,116 @@ def create_and_compile_model(k):
     return model
 
 
-def normalize_data(data):
-    """ Normalize data using MinMaxScaler """
-    scaler = MinMaxScaler()
-    data_normalized = scaler.fit_transform(data)
-    return data_normalized
+def plot_confusion_matrix(cm):
+    ax = sns.heatmap(cm, annot=True)
+    ax.get_figure().savefig('confusion_matrix.png')
 
 
-def main(k: int, train_model: bool):
-    """
-    k: number of clusters/classes
-    train_model: train model or load previous trained model
-    """
+def main(
+        k: int,  # number of classes
+        train_model: bool  # train model or load previous trained model
+    ) -> None:
+    
+    # Protection
     if not train_model and not os.path.exists('best_model.h5'):
         print('ERROR: best_model.h5 file can not be found, run without the --loadBestModel flag for training a model first, exiting')
         sys.exit(1)
     
-    # Create data
-    data, labels = create_data(k, 100000)
-    make_scatter_plot(data, labels, k, 'data.pdf')
+    # Create fake data
+    data, labels = create_data(k=k, npoints=100)
+    make_scatter_plot(data=data, labels=labels, k=k, outname='data.png')
+
+    # Separate data into training, validation and testing data
+    #   30% is used for testing
+    #   20% of the remaining data is used for validation
+    #   the rest is used for training
+    x_train_tmp, X_test, y_train_tmp, y_test = train_test_split(data, labels, test_size=0.3, random_state=10)
+    X_train, X_val, y_train, y_val = train_test_split(x_train_tmp, y_train_tmp, test_size=0.2, random_state=10)
+
+    # Standardize all datasets
+    data_dict = {
+        'training': (X_train, y_train),
+        'testing': (X_test, y_test),
+        'validation': (X_val, y_val),
+    }
+    standardized_data_dict = {}
+    for dataset_type, data_tuple in data_dict.items():
+        data_standardized = standardize_data(data=data_tuple[0])
+        standardized_data_dict[dataset_type] = (data_standardized, data_tuple[1])
+
+    # Visualize standardized training dataset
+    make_scatter_plot(
+        data = standardized_data_dict['training'][0],
+        labels = standardized_data_dict['training'][1],
+        k = k,
+        outname = 'standardized_training_data.png'
+    )
     
+    # Create and compile model (if training was requested)
     if train_model:
-        model = create_and_compile_model(k)
-
-    # Normalize data
-    data_normalized = normalize_data(data)
-    make_scatter_plot(data_normalized, labels, k, 'data_normalized.pdf')
+        model = create_and_compile_model(k=k)
     
-    # Prepare labels
-    labels = labels.reshape(-1, 1)
-    labels = keras.utils.to_categorical(labels, num_classes=k)
-     
-    # Prepare train/test data
-    x_train, x_test, y_train, y_test = train_test_split(data_normalized, labels, test_size = 0.3, random_state=10)
+    # Compare distribution of classes
+    compare_distributions(
+            {
+                'all': labels,
+                'training': y_train,
+                'testing': y_test,
+                'validation': y_val
+            },
+            k
+        ) 
 
+    # Train model or load best trained model (as requested)
     if train_model:
-        # Train model
-        history = train(model, x_train, y_train)
-
-        # Plot loss
-        plot_loss(history)
-
-    # Load best model
-    model = keras.models.load_model('best_model.h5')
+        history = train(
+            model,
+            standardized_data_dict['training'][0],  # training data
+            standardized_data_dict['training'][1],  # training labels
+            standardized_data_dict['validation']  # validation data and labels
+        )
+        plot_loss(history)  # plot loss vs epoch
+    else:  # load best model    
+        model = keras.models.load_model('best_model.h5')
 
     # Evaluate model in test data
-    score = model.evaluate(x_test, y_test, verbose=0)
-    print('Loss in test data:', score[0])
+    score = model.evaluate(
+        standardized_data_dict['testing'][0],  # testing data
+        standardized_data_dict['testing'][1],  # testing labels
+        verbose = 0
+    )
     print('Accuracy in test data:', score[1])
 
-    # Get predicted labels on full data
-    predictions = model.predict(data_normalized)
-    labels_predicted = np.argmax(predictions, axis=1)
-    make_scatter_plot(data_normalized, labels_predicted, k, 'data_predicted.pdf')
+    # Get predicted labels on the test data
+    predictions = model.predict(standardized_data_dict['testing'][0])
+    labels_predicted = np.argmax(predictions, axis=1)  # find label with highest probability
+
+    # Visualize test dataset with true labels
+    make_scatter_plot(
+        data = data_dict['testing'][0],  # data for testing
+        labels = data_dict['testing'][1],  # labels for testing
+        k = k,
+        outname = 'test_data.png'
+    )
+
+    # Visualize test dataset with predicted labels
+    make_scatter_plot(
+        data = data_dict['testing'][0],  # data for testing
+        labels = labels_predicted,  # predicted labels for test data
+        k = k,
+        outname = 'test_data_predicted_labels.png'
+    )
+
+    # Calculate and plot confusion matrix
+    cm = confusion_matrix(data_dict['testing'][1], labels_predicted)
+    plot_confusion_matrix(cm)
 
     print('>>> ALL DONE <<<')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--k', '-k', type=int, action='store', default=3, help='Number of clusters/classes')
+    parser.add_argument('--k', '-k', type=int, action='store', default=3, help='Number of classes')
     parser.add_argument('--loadBestModel', action='store_true', default=False, help='No model will be trained and will load model from best_model.h5')
     args = parser.parse_args()
     main(args.k, not args.loadBestModel)
